e11ce823b1c94977ab51a185228538484ebec117
Lahir-Sai-Vignesh <lahirsaivignesh3@gmail.com>
2024-07-09 10:31:18 +0530

    corrected logical errors in giftRoutes and added next function to use global error handler

--- backend/routes/giftRoutes.js
+++ backend/routes/giftRoutes.js
@@ -4,34 +4,37 @@ import connectToDatabase from "../models/db.js";
 
  const router = express.Router()
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res,next) => {
     console.log('/ called');
     try {
         const db = await connectToDatabase();
         const collection = db.collection("gifts");
         const gifts = await collection.find({}).toArray();
         res.json(gifts);
-    } catch (e) {
-        console.error('Error fetching gifts', e)
-        
+    } catch (err) {
+        console.error('Error fetching gifts', err)
+        next(err)
     }
 });
 
-router.get('/:id',async (req,res)=>{
+router.get('/:id',async (req,res,next)=>{
     try{
         const db= await connectToDatabase();
         const collection = db.collection('gifts');
         const gift_id = req.params.id;
         const gift = await collection.findOne({id:gift_id}).toArray();
+        if (!gift){
+            res.status(404).send("Gift Not Found");
+        }
         res.json(gift);
     }
-    catch(err){
-        res.status(404).send("Gift Not Found");
+    catch(err){   
+        next(err)
     }
 });
 
 
-router.post('/',async (req,res)=>{
+router.post('/',async (req,res,next)=>{
     try{
         const db= await connectToDatabase();
         const collection = db.collection('gifts');
@@ -40,7 +43,8 @@ router.post('/',async (req,res)=>{
         res.send(gift);
     }
     catch(err){
-        console.log("Error Inserting Item")
+        console.log("Error Inserting Item");
+        next(err)
     }
 })
 
