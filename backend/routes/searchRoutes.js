80d5afb3462a3474065f43ae0fee0ebed59ec449
Lahir-Sai-Vignesh <lahirsaivignesh3@gmail.com>
2024-07-09 10:27:09 +0530

    implemented search route in the api

--- backend/routes/searchRoutes.js
+++ backend/routes/searchRoutes.js
@@ -1,9 +1,37 @@
 import express from "express";
+import connectToDatabase from "../models/db";
+
 
 const router = express.Router()
 
-router.get('/',(req,res)=>{
-    res.send("Inside SearchRoutes");
+router.get('/',async(req,res)=>{
+    try{
+        const db = await connectToDatabase();
+        const collection = db.collection('gifts');
+        const query = {}
+
+        // ensuring name exists and also name is not a whitespace
+        if(req.query.name && req.query.name.trim()!==''){
+            query.name = {$regex:req.query.name,$options:"i"}
+            // https://www.mongodb.com/docs/v2.2/reference/operator/query/regex/ (visit this link for syntax reference)
+        }
+        
+        if (req.query.category){
+            query.category = req.query.category;
+        }
+        if (req.query.condition){
+            query.condition = req.query.condition;
+        }
+        if (req.query.age_years){
+            query.age_years = req.query.age_years;
+        }
+
+        const gifts = await collection.find(query).toArray();
+        res.json(gifts);
+    }
+    catch(err){
+        console.err(err)
+    }
 });
 
 export default router;
