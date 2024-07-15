9ac21096f68217c3564e2d8cfcd4b070106e79b8
Lahir-Sai-Vignesh <lahirsaivignesh3@gmail.com>
2024-07-15 12:22:49 +0530

    remved unused useContext import

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -5,6 +5,7 @@ const AppContext = createContext();
 export function AuthProvider({ children }) {
   // if I set it to false whenever I refresh my page is reset to false but I need to set it depending on the authToken to persist isLoggedIn throughout refreshes
   const [isLoggedIn, setIsLoggedIn] = useState(!!sessionStorage.getItem('authToken')); 
+  
   const [userName, setUserName] = useState(sessionStorage.getItem('username'));
 
   return (
