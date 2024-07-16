60fb7b27b073770b79d1bc10798ed420cbf960fd
Lahir-Sai-Vignesh <lahirsaivignesh3@gmail.com>
2024-07-16 19:02:46 +0530

    removed unused useEffect

--- frontend/src/components/SearchPage/SearchPage.js
+++ frontend/src/components/SearchPage/SearchPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import urlConfig from '../../config';
 import axios from 'axios';
